86d4313c67d410f99ccde9f88ad0d5fb5ae6baac
heogden <heogden12@gmail.com>
2023-04-19 11:26:40 +0100

    fill in some detail

--- R/fit_given_sp.R
+++ R/fit_given_sp.R
@@ -1,65 +1,86 @@
 #' @param sp the smoothing parameter
+#' @param sigma the standard deviation of the normal errors
 #' @param kmax the maximum number of variation functions to use
 #' @param nbasis the number of spline basis functions to use
-fit_given_sp <- function(data, sp, kmax, nbasis) {
+fit_given_sp <- function(data, sp, sigma, kmax, nbasis) {
     #' find the basis to use
-    
-    #' fit the mean-only model (k = 0)
+    basis <- find_orthogonal_spline_basis(nbasis, data)
 
+    fits <- list()
+    #' fit the mean-only model (k = 0)
+    fits[[1]] <- fit_0(data, sp, sigma, basis)
+    
     #' fit with k variation functions, fixing mean and first k-1 functions
     for(k in 1:kmax) {
+        fits[[k+1]] <- fit_given_k(data, sp, sigma, k, fits[[k]], basis)
         #' stop early if f_j very close to 0
     }
-    #' (can extract fit with k variation functions by considering subset of
-    #'  whole parameter vector for last fit)
+    fits
 }
 
-#' @param beta_lk k x nbasis matrix with spline coefficients for each of f_0, .., f_{k-1}
-fit_given_k <- function(data, k, beta_lk, basis) {
-    #' k = 0 is mean only
+#' @param k the number of variation functions to use 
+fit_given_k <- function(data, sp, sigma, k, fit_km1, basis) {
     if(k > 1) {
+        beta_lk <- fit_km1$beta
         #' find basis for orthogonal complement of beta_1, ..., beta_km1
+        transform <- find_orthogonal_complement_tranform(beta_lk)
+            
+        #' pre-compute "transformed" spline basis:
+        X_k <- basis$design %*% transform
+        S <- basis$S %*% transform
         
-
     } else {
-        #' no restrictions needed on beta_0 and beta_1
+        #' no restrictions needed on beta_1
+        X_k <- basis$design
+        S <- basis$S
     }
-
-    #' pre-compute "transformed" spline basis:
-    #'   A = spline design (full data) %*% transform mat
-    #'   and S_transform = t(transform mat) %*% S %*% transform mat
     
-    #' optimize loglikelihood for alpha_k, keeping beta_mk fixed
+    #' optimize loglikelihood for alpha_k, keeping f_0, f_1, .., f_{k-1} (and sigma) fixed
+    opt_out <- optim(par_init, find_loglikelihood_k, X_k = X_k, fit_km1 = fit_km1)
 
-    #' within loglikelihood:
-    #'     f_k(t) = A %*% alpha_k (for full data set)
-    #' within penalty:
-    #'   only need to compute expected wiggliness w_k of f_k
-    #'     (others w_0, ..., w_{k-1} already stored and are constants)
-    #'   w_k = t(alpha_k) %*% S_transform %*% alpha_k
-
-    #' to calculate loglikelihood, then need to split by cluster.
-    #' For each cluster, pass in information from fit_km1
-    #' This information will be a list, with one element for each cluster
-    #' So need to pass in relevant part of that list, along with subsets
-    #' of z and f_k for each cluster
+    alpha_k <- opt_out$par
+    f_k <- X_k %*% alpha_k
+    cluster_info <- lapply(fit_km1$cluster_info, f_k = f_k)
     
-    #' As well as returning optimal beta_k, should return other quantities
-    #' which will be needed for next k. Group beta_k and all this extra information
-    #' as fit_k, to be passed on. So actually should pass in fit_km1, rather than beta_lm
-
-    #' Return list of information, with one element for each cluster, including
-    #' (all at optimal beta_k):
-    #'   Sigma_k_inv
-    #'   log(det(Sigma_k))
-    #'   z^T Sigma_k_inv z
-    #'   Sigma_k_inv z
-
-
-    #' Globally (not separately for each cluster), also need
-    #'   expected wiggliness of f_0, ..., f_k
-    #'   z = y - mu (can be found after k = 0, once we know mu, then passed on)
-
-
+    beta_k <- transform %*% alpha_k
+    beta <- cbind(fit_km1$beta, beta_k)
+    
+    list(beta_0 = fit_km1$beta_0, beta = beta, cluster_info = cluster_info)
 }
+
+#' Fit the mean-only mode, and set up for use in future fits
+fit_0 <- function(data, sp, sigma, basis) {
+    X_0 <- basis$design
+    mod_0 <- lm.fit(X_0, data$y)
+
+    clusters <- unique(data$c)
+    cluster_info <- lapply(clusters, init_cluster_info)
+    
+    list(k = 0,
+         beta_0 = mod_0$coefficients,
+         beta = matrix(nrow = ncol(X_0), ncol = 0),
+         cluster_info = cluster_info)
+}
+
+init_cluster_info <- function(cluster, data, sigma) {
+    rows <- which(data$c == cluster)
+    n_c <- length(rows)
+    Sigma <- diag(sigma^2, nrow = n_c, ncol = n_c)
+    Sigma_inv <- 1 / Sigma
+    ldet_Sigma <- 2 * n_c * log(sigma)
+    z <- mod_0$residuals[rows]
+    Sigma_inv_z <- z / sigma^2
+    tz_Sigma_inv_z <- sum(z^2) / sigma^2
+    list(cluster = cluster, rows = rows, Sigma_inv = Sigma_inv,
+         ldet_Sigma = ldet_Sigma, z = z, Sigma_inv_z = Sigma_inv_z,
+         tz_Sigma_inv_z = tz_Sigma_inv_z)
+}
+
+
+update_cluster_info <- function(cluster_info_km1, f_k) {
+    a <- f_k[cluster_info_km1$rows]
+    find_info_k(a, cluster_info_km1)
+}
+
+
 
---
+++ R/loglikelihood.R
@@ -0,0 +1,50 @@
+#' Find log phi(z; 0, Sigma_k),
+#' where z is fixed and stored
+#' and
+#' Sigma_k = Sigma_km1 + a a^T
+#' for each new value of a.
+#' Here info_km1 contains values needed corresponding to Sigma_km1 and z
+ldmvnorm <- function(a, info_km1) {
+    b <- info_km1$Sigma_inv %*% a
+    c <- 1 + sum(a * b)
+    ldet_Sigma <- log(c) + info_km1$ldet_Sigma
+    d <- sum(info_km1$Sigma_inv_z * a)
+
+    n <- length(info_km1$z)
+
+    quad_form <- info_km1$tz_Sigma_inv_z - d^2/c
+    -n/2 * log(2*pi) - 1/2 * ldet_Sigma - 1/2 * quad_form
+}
+
+find_info_k <- function(a, info_km1) {
+    b <- as.numeric(info_km1$Sigma_inv %*% a)
+    c <- 1 + sum(a * b)
+    ldet_Sigma <- log(c) + info_km1$ldet_Sigma
+    Sigma_inv <- info_km1$Sigma_inv - tcrossprod(b, b) / c
+    z <- imfo_km1$z
+    Sigma_inv_z <- as.numeric(Sigma_inv %*% z)
+    tz_Sigma_inv_z <- sum(z * Sigma_inv_z)
+    list(cluster = info_km1$cluster, rows = info_km1$rows,
+         Sigma_inf = Sigma_inv, ldet_Sigma = ldet_Sigma,
+         z = z, Sigma_inv_z = Sigma_inv_z, tz_Sigma_inv_z = tz_Sigma_inv_z)
+    
+}
+
+
+find_loglikelihood_k <- function(alpha_k, X_k, fit_km1) {
+    f_k <- X_k %*% alpha_k
+    l_comp <- c()
+    for(c in seq_along(fit_km1)) {
+        fit_km1_c <- fit_km1[[c]]
+        l_comp[c] <- ldmvnorm(f_k[fit_km1_c$rows], fit_km1_c)
+    }
+    sum(l_comp)
+}
+
+find_wiggliness_f_k <- function(alpha_k, S_k) {
+    emulator::quad.form(S_k, alpha_k)
+}
+
+find_pen_loglikelihood_k <- function(alpha_k, X_k, S_k, fit_km1) {
+    find_loglikelihood_k(alpha_k, X_k, fit_km1) - find_wiggliness_f_k(alpha_k, S_k)
+}
