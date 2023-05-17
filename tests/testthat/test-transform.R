754dd77c2ca748117c46b0a85f6841cfaecb235b
heogden <heogden12@gmail.com>
2023-05-17 13:21:22 +0100

    find derivs of beta wrt alpha using dT array

--- tests/testthat/test-transform.R
+++ tests/testthat/test-transform.R
@@ -56,5 +56,41 @@ test_that("can differentiate transform", {
     dT_man <- numDeriv::jacobian(find_orthogonal_complement_transform, beta)
     dT <- dH1[, -1, ]
     expect_equal(as.numeric(dT), as.numeric(dT_man))
- 
+
+    find_beta_from_alpha <- function(alpha, nbasis, k) {
+        alpha_split <- split_alpha(alpha, nbasis, k)
+        find_beta(alpha_split)
+    }
+    set.seed(1)
+    alpha <- rnorm(3 * nbasis - 1)
+    k <- 2
+    beta <- find_beta_from_alpha(alpha, nbasis, k)
+
+    #' this is what we want to end up with
+    #' (ignore first nbasis elements, giving alpha_0, they just control beta_0)
+    dbeta_man <- numDeriv::jacobian(find_beta_from_alpha, alpha, nbasis = nbasis, k = k)[,-(1:nbasis)]
+    #' beta_1 influenced by alpha_1 only (in fact beta_1 = alpha_1). So deriv is identity
+    #' beta_2 influenced by alpha_1 and alpha_2
+    #' Focus on this part derivatives of beta_2 write alpha = (alpha_1, alpha_2)
+    dbeta2_man <- dbeta_man[-(1:nbasis), ]
+
+    T <- find_orthogonal_complement_transform(beta[, 1, drop = FALSE])
+    alpha_12 <- alpha[-(1:nbasis)]
+    alpha_1 <- alpha_12[1:nbasis]
+    alpha_2 <- alpha_12[-(1:nbasis)]
+    beta_2 <- T %*% alpha_2
+
+    #' derivative of beta_2 wrt alpha_2 is just T
+    #' so what is left is derivatives of beta_2 wrt alpha_1
+    #' T is a function of beta_1 = alpha_1
+    dH1 <- find_dH1(beta[, 1, drop = FALSE])
+    dT <- dH1[, -1, ]
+    
+    dbeta2_alpha1 <- matrix(nrow = nbasis, ncol = nbasis)
+    for(i in 1:nbasis)
+        for(j in 1:nbasis)
+            dbeta2_alpha1[i,j] <- sum(dT[i,,j] * alpha_2)
+
+    dbeta2_alpha1_man <- dbeta2_man[,1:nbasis]
+    expect_equal(dbeta2_alpha1, dbeta2_alpha1_man)
 })
