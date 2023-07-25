40712c912595462cd42befd1edbe463c419aeade
heogden <heogden12@gmail.com>
2023-07-25 14:16:21 +0100

    note small sp estimate for some straight lines

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -26,6 +26,7 @@ test_that("sensible fit for test data 1 (straight lines)", {
         facet_wrap(vars(c))
 })
 
+
 test_that("sensible fit for test data 2 (not straight lines)", {
     data_full <- generate_test_data_2()
     data <- data_full$data
@@ -60,6 +61,41 @@ test_that("sensible fit for test data 2 (not straight lines)", {
         geom_line(aes(y = eta), linetype = "dashed") +
         facet_wrap(vars(c))
 })
+
+test_that("chooses a reasonably large sp if have straight lines", {
+    sigma_u <- 0.5
+    sigma <- 0.1
+    n_clusters <- 50
+    n_obs_per_cluster <- 10
+    beta_0 <- -1
+    beta_1 <- 2
+
+    set.seed(1)
+
+    u <- rnorm(n_clusters, sd = sigma_u)
+
+    x_full <- seq(0, 1, length.out = 1000)
+    x_index <- sample(seq_along(x_full), n_clusters * n_obs_per_cluster, replace = TRUE)
+
+    library(tidyverse)
+
+    data <- bind_cols(x_index = x_index,
+                      c = rep(1:n_clusters, each = n_obs_per_cluster)) %>%
+        mutate(x = x_full[x_index],
+               eta = (beta_0 + beta_1 * x + u[c]),
+               epsilon = rnorm(length(x_index), 0, sigma),
+               y = eta + epsilon)
+
+    eta_data <- crossing(x = x_full,
+                         c = 1:n_clusters) %>%
+        mutate(eta = (beta_0 + beta_1 * x + u[c]))
+
+    mod <- fit_flexl(data)
+
+    expect_gt(mod$sp, 0.1)
+})
+
+
 
 test_that("error if data has missing values", {
     library(ALA)
