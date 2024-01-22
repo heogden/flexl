b47f37a065fb342ac63ef25074e2511f97669c76
heogden <heogden12@gmail.com>
2024-01-22 09:37:53 +0000

    remove tidyverse dependencies

--- R/flexl-package.R
+++ R/flexl-package.R
@@ -1,8 +1,5 @@
 ## usethis namespace: start
 #' @import RcppEigen
-#' @import rlang
-#' @importFrom glue glue
-#' @importFrom lifecycle deprecated
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib flexl, .registration = TRUE
 ## usethis namespace: end
