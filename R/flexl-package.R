056e34a4ee990dfc89ec4e7ea61d0a42a2a95fd7
heogden <heogden12@gmail.com>
2025-01-13 13:50:38 +0000

    add setup from RcppParallel

--- R/flexl-package.R
+++ R/flexl-package.R
@@ -1,6 +1,7 @@
 ## usethis namespace: start
 #' @import RcppEigen
 #' @importFrom Rcpp sourceCpp
+#' @importFrom RcppParallel RcppParallelLibs
 #' @useDynLib flexl, .registration = TRUE
 ## usethis namespace: end
 NULL
